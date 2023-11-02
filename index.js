8a9a9d58fdf3ec8b035c8af7c661ea6dea443702
Akilesh Subhadu <86011577+Yohan4@users.noreply.github.com>
2023-11-02 12:11:46 +0400

    feat: ADD register page and modify  other pages

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function redirectToLogin() {
     window.location.href = '/pages/login.html';
 }
+
+const placeholder = () => {
+    const dob = document.getElementById('dob')
+    if (dob.value === ''){
+        dob.type = 'text';
+        dob.placeholder = 'Date of birth';
+    }
+};
+
+const changePlaceholderType = () => {
+    const dob = document.getElementById('dob');
+    dob.type ='date';
+};
+
+
